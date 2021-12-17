6ba0e2bbb8f7d499448e6e00a220456b0cd36ec6
Kenji Saito <1867845+poad@users.noreply.github.com>
2021-12-17 09:17:34 +0900

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    webpack5: true,
+    reactStrictMode: true,
+    swcLoader: true,
+    swcMinify: true,
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import { Box, Typography, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, useTheme, tableCellClasses } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import Layout from '../components/Layout';
+
+interface Column {
+  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  label: string;
+  minWidth?: number;
+  align?: 'right';
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toFixed(2),
+  },
+];
+
+interface Data {
+  name: string;
+  code: string;
+  population: number;
+  size: number;
+  density: number;
+}
+
+function createData(
+  name: string,
+  code: string,
+  population: number,
+  size: number,
+): Data {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+
+const Home = (): JSX.Element => {
+  const theme = useTheme();
+  return (
+    <Layout>
+      <Box component="main" sx={{ width: '100%', flexGrow: 1 }}>
+        <Box sx={{
+          margin: '0',
+          width: '100%',
+          paddingTop: '80px',
+          lineHeight: '1.15',
+        }}>
+          <Typography
+            component='h1'
+            align='center'
+            fontSize='48px'
+            color={theme.palette.text.primary}
+          >
+            mui with sticky table
+          </Typography>
+        </Box>
+
+        <Paper sx={{ width: '100%', overflow: 'revert' }} elevation={0}>
+          <TableContainer sx={{ height: 'calc(100vh - 64px)', overflow: 'revert' }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <StyledTableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </StyledTableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows
+                  .map((row) => {
+                    return (
+                      <StyledTableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <StyledTableCell key={column.id} align={column.align} component={'th'}>
+                              {column.format && typeof value === 'number'
+                                ? column.format(value)
+                                : value}
+                            </StyledTableCell>
+                          );
+                        })}
+                      </StyledTableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Box>
+    </Layout>
+  );
+};
+
+export default Home;
